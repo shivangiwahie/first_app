2450e8a67d0012f1a9fe41ba9d8024ad5b2e9725
shivangiwahie <shivangi.wahie@icreon.net>
2011-09-21 16:43:30 +0530

    vinay

---
+++ vinay/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vinay::Application.config.secret_token = 'b31736e27dc5e23676849c14e35abeb28d3a6cf1547182644a65e5ba01e542cee64e6cb53d2e0f7fafc57f2fbe253706840cecd21b38fa4d4c6ec975bc0b6cee'
